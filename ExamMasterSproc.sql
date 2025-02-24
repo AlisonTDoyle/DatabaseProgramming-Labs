cad489da073300d1d44358ca0aead97892bb0523
S00233102 <s00233102@atu.ie>
2025-02-24 19:29:43 +0000

    :sparkles: Added master sproc

---
+++ ExamMasterSproc.sql
@@ -0,0 +1,13 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER proc [dbo].[ExamMaster]
+-- external variables
+as
+-- internal variables
+-- read data and populate internal vars
+-- business logic
+-- subsprocs
+-- success message
+GO
