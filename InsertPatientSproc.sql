5751b65111912dde4982b43696386b0a1c647b81
S00233102 <s00233102@atu.ie>
2025-02-25 14:50:36 +0000

    Can insert patient using subsproc

--- InsertPatientSproc.sql
+++ InsertPatientSproc.sql
@@ -3,5 +3,20 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER proc [dbo].[InsertPatient]
-as
-GO
+-- external variables
+@EPatientFirstName varchar(35)
+, @EPatientLastName varchar(35)
+, @EWardId int
+, @ECovidStatus char(8)
+AS
+-- business logic
+BEGIN TRY
+INSERT INTO dbo.PatientTBL 
+(PatientFname, PatientLname, PatientWarD, PatientCOVIDStatus)
+VALUES
+(@EPatientFirstName, @EPatientLastName, @EWardId, @ECovidStatus)
+END TRY
+BEGIN CATCH
+;throw
+END CATCH
+RAISERROR ('Patient has been inserted', 16, 1)
